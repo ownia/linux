8676e25b09dade3fffca5d1b334f1c35a26571c7
James Morse <james.morse@arm.com>
2024-11-20 15:49:06 +0000

    fs/resctrl: Add additional files for percentage and bitmap controls
    MPAM has cache capacity controls that effectively take a percentage.
    Resctrl supports percentages, but the collection of files that are
    exposed to describe this control belong to the MB resource.
    
    To find the minimum granularity of the percentage cache capacity controls,
    user-space is expected to rad the banwdidth_gran file, and know this has
    nothing to do with bandwidth.
    
    The only problem here is the name of the file. Add duplicates of these
    properties with percentage and bitmap in the name. These will be exposed
    based on the schema format.
    
    The existing files must remain tied to the specific resources so that
    they remain visible to user-space. Using the same helpers ensures the
    values will always be the same regardless of the file used.
    
    These files are not exposed until the new RFTYPE schema flags are
    set on a resource 'fflags'.
    
    Signed-off-by: James Morse <james.morse@arm.com>

--- fs/resctrl/rdtgroup.c
+++ fs/resctrl/rdtgroup.c
@@ -1938,11 +1938,25 @@ static struct rftype res_common_files[] = {
 		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_RES_CACHE,
 	},
 	{
+		.name		= "bitmap_mask",
+		.mode		= 0444,
+		.kf_ops		= &rdtgroup_kf_single_ops,
+		.seq_show	= rdt_default_ctrl_show,
+		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_SCHEMA_BITMAP,
+	},
+	{
 		.name		= "min_cbm_bits",
 		.mode		= 0444,
 		.kf_ops		= &rdtgroup_kf_single_ops,
 		.seq_show	= rdt_min_cbm_bits_show,
 		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_RES_CACHE,
+	},
+	{
+		.name		= "bitmaps_min_bits",
+		.mode		= 0444,
+		.kf_ops		= &rdtgroup_kf_single_ops,
+		.seq_show	= rdt_min_cbm_bits_show,
+		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_SCHEMA_BITMAP,
 	},
 	{
 		.name		= "shareable_bits",
@@ -1966,11 +1980,25 @@ static struct rftype res_common_files[] = {
 		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_RES_MB,
 	},
 	{
+		.name		= "percent_min",
+		.mode		= 0444,
+		.kf_ops		= &rdtgroup_kf_single_ops,
+		.seq_show	= rdt_min_bw_show,
+		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_SCHEMA_PERCENT,
+	},
+	{
 		.name		= "bandwidth_gran",
 		.mode		= 0444,
 		.kf_ops		= &rdtgroup_kf_single_ops,
 		.seq_show	= rdt_bw_gran_show,
 		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_RES_MB,
+	},
+	{
+		.name		= "percent_gran",
+		.mode		= 0444,
+		.kf_ops		= &rdtgroup_kf_single_ops,
+		.seq_show	= rdt_bw_gran_show,
+		.fflags		= RFTYPE_CTRL_INFO | RFTYPE_SCHEMA_PERCENT,
 	},
 	{
 		.name		= "delay_linear",
