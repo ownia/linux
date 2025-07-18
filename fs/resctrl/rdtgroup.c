kernelorg-mirror/linux_kernel_git_morse_linux

fs/resctrl: Add a schema format to the schema, allowing it to be different
parse_bw() doesn't bother checking the bounds it is given if the
resource is in use by mba_sc. This is because the values parsed from
user-space are not the same as those the control should take.

To make this disparity easier to work with, a second different copy
of the schema format is needed, which would need a version of
resctrl_get_default_ctrl(). This would let the resctrl change the
schema format presented to user-space, provided it converts it to match
what the architecture code expects.

Add a second schema format for use with mba_sc. The membw properties
are copied and the schema version is used. When mba_sc is enabled
the schema copy of these properties is modified.

Signed-off-by: James Morse <james.morse@arm.com>

fs/resctrl/rdtgroup.c
@@ -1075,9 +1075,8 @@ static int rdt_default_ctrl_show(struct kernfs_open_file *of,
 				 struct seq_file *seq, void *v)
 {
 	struct resctrl_schema *s = of->kn->parent->priv;
-	struct rdt_resource *r = s->res;
 
-	seq_printf(seq, "%x\n", resctrl_get_resource_default_ctrl(r));
+	seq_printf(seq, "%x\n", resctrl_get_schema_default_ctrl(s));
 	return 0;
 }
 
@@ -1201,9 +1200,8 @@ static int rdt_min_bw_show(struct kernfs_open_file *of,
 			   struct seq_file *seq, void *v)
 {
 	struct resctrl_schema *s = of->kn->parent->priv;
-	struct rdt_resource *r = s->res;
 
-	seq_printf(seq, "%u\n", r->membw.min_bw);
+	seq_printf(seq, "%u\n", s->membw.min_bw);
 	return 0;
 }
 
@@ -1236,9 +1234,8 @@ static int rdt_bw_gran_show(struct kernfs_open_file *of,
 			    struct seq_file *seq, void *v)
 {
 	struct resctrl_schema *s = of->kn->parent->priv;
-	struct rdt_resource *r = s->res;
 
-	seq_printf(seq, "%u\n", r->membw.bw_gran);
+	seq_printf(seq, "%u\n", s->membw.bw_gran);
 	return 0;
 }
 
@@ -2587,7 +2584,22 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	if (cl > max_name_width)
 		max_name_width = cl;
 
-	switch (r->schema_fmt) {
+	s->schema_fmt = r->schema_fmt;
+	s->membw = r->membw;
+
+	/*
+	 * When mba_sc() is enabled the format used by user space is different
+	 * to that expected by hardware. The conversion is done by
+	 * update_mba_bw().
+	 */
+	if (is_mba_sc(r)) {
+		s->schema_fmt = RESCTRL_SCHEMA_RANGE;
+		s->membw.min_bw = 0;
+		s->membw.max_bw = MBA_MAX_MIBPS;
+		s->membw.bw_gran = 1;
+	}
+
+	switch (s->schema_fmt) {
 	case RESCTRL_SCHEMA_BITMAP:
 		s->fmt_str = "%d=%x";
 		break;

ownia/linux

fs/resctrl/rdtgroup.c
@@ -1104,9 +1104,8 @@ static int rdt_default_ctrl_show(struct kernfs_open_file *of,
 				 struct seq_file *seq, void *v)
 {
 	struct resctrl_schema *s = rdt_kn_parent_priv(of->kn);
-	struct rdt_resource *r = s->res;
 
-	seq_printf(seq, "%x\n", resctrl_get_resource_default_ctrl(r));
+	seq_printf(seq, "%x\n", resctrl_get_schema_default_ctrl(s));
 	return 0;
 }
 
@@ -1230,9 +1229,8 @@ static int rdt_min_bw_show(struct kernfs_open_file *of,
 			   struct seq_file *seq, void *v)
 {
 	struct resctrl_schema *s = rdt_kn_parent_priv(of->kn);
-	struct rdt_resource *r = s->res;
 
-	seq_printf(seq, "%u\n", r->membw.min_bw);
+	seq_printf(seq, "%u\n", s->membw.min_bw);
 	return 0;
 }
 
@@ -1265,9 +1263,8 @@ static int rdt_bw_gran_show(struct kernfs_open_file *of,
 			    struct seq_file *seq, void *v)
 {
 	struct resctrl_schema *s = rdt_kn_parent_priv(of->kn);
-	struct rdt_resource *r = s->res;
 
-	seq_printf(seq, "%u\n", r->membw.bw_gran);
+	seq_printf(seq, "%u\n", s->membw.bw_gran);
 	return 0;
 }
 
@@ -2628,7 +2625,22 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	if (cl > max_name_width)
 		max_name_width = cl;
 
-	switch (r->schema_fmt) {
+	s->schema_fmt = r->schema_fmt;
+	s->membw = r->membw;
+
+	/*
+	 * When mba_sc() is enabled the format used by user space is different
+	 * to that expected by hardware. The conversion is done by
+	 * update_mba_bw().
+	 */
+	if (is_mba_sc(r)) {
+		s->schema_fmt = RESCTRL_SCHEMA_RANGE;
+		s->membw.min_bw = 0;
+		s->membw.max_bw = MBA_MAX_MBPS;
+		s->membw.bw_gran = 1;
+	}
+
+	switch (s->schema_fmt) {
 	case RESCTRL_SCHEMA_BITMAP:
 		s->fmt_str = "%d=%x";
 		break;