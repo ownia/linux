kernelorg-mirror/linux_kernel_git_morse_linux

fs/resctrl: Remove 'range' schema format
Resctrl previously had a 'range' schema format that took some kind of
number. This has since been split into percentage, MB/s and an AMD
platform specific scheme.

As range is no longer used, remove it.

The last user is mba_sc which should be described as taking MB/s.

Signed-off-by: James Morse <james.morse@arm.com>

fs/resctrl/rdtgroup.c
@@ -2593,7 +2593,7 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	 * update_mba_bw().
 	 */
 	if (is_mba_sc(r)) {
-		s->schema_fmt = RESCTRL_SCHEMA_RANGE;
+		s->schema_fmt = RESCTRL_SCHEMA_MIBPS;
 		s->membw.min_bw = 0;
 		s->membw.max_bw = MBA_MAX_MIBPS;
 		s->membw.bw_gran = 1;
@@ -2603,7 +2603,6 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	case RESCTRL_SCHEMA_BITMAP:
 		s->fmt_str = "%d=%x";
 		break;
-	case RESCTRL_SCHEMA_RANGE:
 	case RESCTRL_SCHEMA_PERCENT:
 	case RESCTRL_SCHEMA_MIBPS:
 	case RESCTRL_SCHEMA__AMD_MBA:

ownia/linux

fs/resctrl/rdtgroup.c
@@ -2634,7 +2634,7 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	 * update_mba_bw().
 	 */
 	if (is_mba_sc(r)) {
-		s->schema_fmt = RESCTRL_SCHEMA_RANGE;
+		s->schema_fmt = RESCTRL_SCHEMA_MBPS;
 		s->membw.min_bw = 0;
 		s->membw.max_bw = MBA_MAX_MBPS;
 		s->membw.bw_gran = 1;
@@ -2644,7 +2644,6 @@ static int schemata_list_add(struct rdt_resource *r, enum resctrl_conf_type type
 	case RESCTRL_SCHEMA_BITMAP:
 		s->fmt_str = "%d=%x";
 		break;
-	case RESCTRL_SCHEMA_RANGE:
 	case RESCTRL_SCHEMA_PERCENT:
 	case RESCTRL_SCHEMA_MBPS:
 	case RESCTRL_SCHEMA__AMD_MBA: